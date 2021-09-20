91a7e5e9f973cb1ac24b27a7b39db0c789404a43
Muhammad Hammad Sani <m.hammadsaani@gmail.com>
2021-09-21 01:57:32 +0500

    Testing

--- Arrays/arrayPractice.java
+++ Arrays/arrayPractice.java
@@ -1,19 +1,21 @@
 package Arrays;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class arrayPractice {
     public static void main(String[] args) {
-        Scanner in  = new Scanner(System.in);
-        int[][] twoD = new int[3][2];
-        for (int i=0; i < twoD.length; i++){
-            for (int j=0; j < twoD[i].length; j++){
-                twoD[i][j] = in.nextInt();
-            }
+        int [] nums = {0,1,2,3,4};
+        int[] index = {0,1,2,2,1};
+        int[] target = new int[nums.length];
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i=0; i < nums.length; i++){
+            result.add(index[i], nums[i]);
         }
-        for (int[] ints : twoD) {
-            System.out.println(Arrays.toString(ints));
+        for (int i=0; i < nums.length; i++){
+            target[i] = result.get(i);
         }
+        System.out.println(result);
     }
 }
